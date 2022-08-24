4f8fae0a62fdf459841c036c8eb2607a62a6fc79
AghayevSehran <sehranagayev89@gmail.com>
2022-08-25 01:23:12 +0400

    save

---
+++ Test/default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Test._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-3.6.0.min.js"></script>
+    <script src="Scripts/jquery.maskedinput.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.phoneClass').change(function () {
+                $(".phoneClass").mask("(800) 999-9999");
+            });
+        });
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblPhone" runat="server" Text="Label">Phone: </asp:Label>
+            <asp:TextBox ID="txtPhone" CssClass="phoneClass" runat="server"></asp:TextBox>
+            <asp:CustomValidator runat="server" Display="Dynamic" ID="phoneValidator"
+                ForeColor="Red" ErrorMessage="" ValidationGroup="" OnServerValidate="phoneValidator_ServerValidate">  
+            </asp:CustomValidator>
+              <asp:Label ID="Label1" runat="server" ForeColor ="Red" Visible="false" Text="Label"> </asp:Label>
+            <div style="margin-left: 50px; margin-top: 5px;">
+                <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+            </div>
+
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ Test/default.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack) Label1.Visible = false;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string inputpath = Server.MapPath("~/App_Data/phoneNumbers.txt");
+                string outputpath = Server.MapPath("~/App_Data/phoneNumberso.txt");
+                bool fileIsEmpty = true;
+                bool alreadyExist = false;
+                bool added = false;
+                using (var writer = new StreamWriter(outputpath))
+                {
+                    foreach (var line in File.ReadLines(inputpath))
+                    {
+                        if (line == "") continue;
+                        fileIsEmpty = false;
+                        if (String.Compare(txtPhone.Text, line) == 0)
+                        {
+                            alreadyExist = true;
+                            break;
+                        }
+                        else if (!added && String.Compare(txtPhone.Text, line) < 0)
+                        {
+                            added = true;
+                            writer.WriteLine();
+                            writer.Write(txtPhone.Text);
+                            writer.WriteLine();
+
+                        }
+                        else if (String.Compare(txtPhone.Text, line) > 0)
+                        {
+                            writer.WriteLine();
+                        }
+                        else if (added)
+                        {
+                            writer.WriteLine();
+                        }
+
+                        writer.Write(line);
+
+                    }
+                    if ((fileIsEmpty || !alreadyExist) && !added)
+                    {
+                        writer.WriteLine();
+                        writer.Write(txtPhone.Text);
+                    }
+                }
+                if (!alreadyExist)
+                {
+                    File.Delete(inputpath);
+                    File.Move(outputpath, inputpath);
+                }
+                else
+                {
+                    File.Delete(outputpath);
+                }
+                if (alreadyExist)
+                {
+                    Label1.Visible = true;
+                    Label1.Text = "Duplicated number.";
+                }
+                else
+                {
+                    Label1.Visible = false;
+                }
+            }
+
+
+        }
+
+        private void Val_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            phoneValidator.ErrorMessage = "Duplicated number.";
+            args.IsValid = false;
+        }
+
+        protected void phoneValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (txtPhone.Text == string.Empty)
+            {
+                phoneValidator.ErrorMessage = "Please enter valide phone number.";
+                args.IsValid = false;
+            }
+            else if (txtPhone.Text.Length != 14)
+            {
+                phoneValidator.ErrorMessage = "Phone Number should be 10 digit.";
+                args.IsValid = false;
+            }
+            else if (!Regex.IsMatch(txtPhone.Text, "^\\+?(\\d[\\d-. ]+)?(\\([\\d-. ]+\\))?[\\d-. ]+\\d$"))
+            {
+                phoneValidator.ErrorMessage = "Phone Number should be valid.";
+                args.IsValid = false;
+            }
+        }
+    }
+}
